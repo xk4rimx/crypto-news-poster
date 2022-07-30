f8cabaa4556861de7a88055d63064b695f83ba57
k4rim <xxkrimgamerxx2@gmail.com>
2022-07-30 20:57:41 +0200

    Add stripping @ from username

--- helpers/channel_last_posts.py
+++ helpers/channel_last_posts.py
@@ -4,6 +4,8 @@ import bs4
 
 def channel_last_posts(username: str) -> list:
 
+    username = username.strip("@")
+
     html = requests.get(f"https://t.me/s/{username}").text
     soup = bs4.BeautifulSoup(html, "lxml")
 
