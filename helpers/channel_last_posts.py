82eba13a16741f8dd489ddbe91f13b2de7de0049
k4rim <xxkrimgamerxx2@gmail.com>
2022-07-30 15:17:51 +0200

    Create channel_last_posts.py

---
+++ helpers/channel_last_posts.py
@@ -0,0 +1,15 @@
+import requests
+import bs4
+
+
+def channel_last_posts(username: str) -> list:
+
+    html = requests.get(f"https://t.me/s/{username}").text
+    soup = bs4.BeautifulSoup(html, "lxml")
+
+    results = soup.find_all(
+        "div", {"class": "tgme_widget_message_text js-message_text"}
+    )
+
+    posts = [r.text for r in results]
+    return posts
