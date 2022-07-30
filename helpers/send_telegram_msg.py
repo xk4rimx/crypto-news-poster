17a24da01684159a8735d12826cda1f966b107ac
k4rim <xxkrimgamerxx2@gmail.com>
2022-07-30 20:55:03 +0200

    Add force-adding "@" to username feature

--- helpers/send_telegram_msg.py
+++ helpers/send_telegram_msg.py
@@ -1,12 +1,15 @@
 import requests
 
 
-def send_telegram_msg(bot_token: str, chat_id: int, text: str) -> None:
+def send_telegram_msg(bot_token: str, username: str, text: str) -> None:
+
+    if not username.startswith("@"):
+        username = "@" + username
 
     response = requests.post(
         f"https://api.telegram.org/bot{bot_token}/sendMessage",
         json={
-            "chat_id": chat_id,
+            "chat_id": username,
             "text": text,
             "parse_mode": "Markdown",
         },
