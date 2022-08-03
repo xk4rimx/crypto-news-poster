db91657be701b23eb0202e90ff67bd6e3aec5e8a
k4rim <xxkrimgamerxx2@gmail.com>
2022-08-03 03:32:22 +0200

    Rename channel_last_posts to telegram_last_posts

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,3 +1,3 @@
 from .last_news_articles import last_news_articles
-from .channel_last_posts import channel_last_posts
+from .telegram_last_posts import telegram_last_posts
 from .send_telegram_msg import send_telegram_msg
---
+++ helpers/telegram_last_posts.py
@@ -0,0 +1,20 @@
+import requests
+import bs4
+
+
+def telegram_last_posts(username: str) -> list:
+
+    username = username.strip("@")
+
+    response = requests.get(f"https://t.me/s/{username}")
+    response.raise_for_status()
+
+    html = response.text
+    soup = bs4.BeautifulSoup(html, "lxml")
+
+    results = soup.find_all(
+        "div", {"class": "tgme_widget_message_text js-message_text"}
+    )
+
+    posts = [r.text for r in results]
+    return posts
