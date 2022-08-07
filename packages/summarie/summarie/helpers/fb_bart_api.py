9766c93edcea6fbcffb15b801f717988e1eb8484
k4rim <xxkrimgamerxx2@gmail.com>
2022-08-08 00:27:53 +0200

    Add waiting for model to load feature

--- packages/summarie/summarie/helpers/fb_bart_api.py
+++ packages/summarie/summarie/helpers/fb_bart_api.py
@@ -5,7 +5,10 @@ def fb_bart_api(inputs: str) -> str:
 
     response = requests.post(
         "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
-        json={"inputs": inputs},
+        json={
+            "inputs": inputs,
+            "config": {"wait_for_model": True},
+        },
     )
 
     response.raise_for_status()
