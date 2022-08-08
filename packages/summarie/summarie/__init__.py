f4db14b1e673aed58131afaf1e25904e9907090c
k4rim <xxkrimgamerxx2@gmail.com>
2022-08-08 20:00:09 +0200

    Add text cleaning and lowercasing

--- packages/summarie/summarie/__init__.py
+++ packages/summarie/summarie/__init__.py
@@ -1,13 +1,28 @@
 from . import helpers
+
+import re
 import newspaper
+import cleantext
+
+
+def _clean_text(text: str) -> str:
+
+    text = cleantext.clean(text)  # replace non-ascii to closest ascii
+    text = text.replace('"', "")  # remove quotes
+    text = re.sub(r"\([^()]*\)", "", text)  # remove text between brackets
+    text = cleantext.clean(text)  # general cleaning
+
+    return text
 
 
 def from_text(text: str) -> str:
 
     """Generates a summary from the given text"""
 
+    text = _clean_text(text)
+
     results = helpers.fb_bart_api(inputs=text)
-    summary = results[0]["summary_text"]
+    summary = results[0]["summary_text"].lower()
 
     return summary
 
