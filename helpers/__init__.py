24d82fb777dc256d577d1d88e1ee097f1a28f360
k4rim <xxkrimgamerxx2@gmail.com>
2022-08-21 21:15:52 +0400

    Reorder imports alphabetically

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,3 +1,3 @@
 from .last_news_articles import last_news_articles
-from .telegram_last_posts import telegram_last_posts
 from .send_telegram_msg import send_telegram_msg
+from .telegram_last_posts import telegram_last_posts
